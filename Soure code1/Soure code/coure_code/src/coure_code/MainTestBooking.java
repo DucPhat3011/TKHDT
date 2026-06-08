89389451bbfe716a7a0c97c413695a7ef1f41fac
YenNgoc17 <24130207@st.hcmuaf.edu.vn>
2026-06-08 15:01:24 +0700

    Add files via upload

--- Soure code1/Soure code/coure_code/src/coure_code/MainTestBooking.java
+++ Soure code1/Soure code/coure_code/src/coure_code/MainTestBooking.java
@@ -1,4 +1,13 @@
+import javax.swing.SwingUtilities;
+
 
 public class MainTestBooking {
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            BookingManager bookingManager = new BookingManager();
+            BookingController bookingController = new BookingController(bookingManager);
+            BookingView bookingView = new BookingView(bookingController);
+            bookingView.render();
+        });
+    }
 }
