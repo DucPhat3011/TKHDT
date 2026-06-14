802cfca07d20078aefb6f9de21ecd1a1bfd01ece
24130329-huyentrang <24130329@st.hcmuaf.edu.vn>
2026-06-14 22:20:16 +0700

    Update IReport.java

--- Soure code1/Soure code/coure_code/src/coure_code/IReport.java
+++ Soure code1/Soure code/coure_code/src/coure_code/IReport.java
@@ -1,5 +1,9 @@
 import java.io.File;
 
+/**
+ * interface chung cho tat ca cac loai bao cao..
+ * dong vai tro Component trong Composite Pattern.
+ */
 public interface IReport {
 	public void generate();
 	public File export(String format);
