f20fe1a853fba192b2f72b4a98ec98d614954113
24130329-huyentrang <24130329@st.hcmuaf.edu.vn>
2026-06-14 22:15:17 +0700

    Update ReportView.java

--- Soure code1/Soure code/coure_code/src/coure_code/ReportView.java
+++ Soure code1/Soure code/coure_code/src/coure_code/ReportView.java
@@ -1,12 +1,28 @@
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ReportView extends JFrame {
 	private ReportController reportController;
+	
+	public ReportView(ReportController reportController) {
+        this.reportController = reportController;
+    }
 
 	public void displayReport(Report reportData) {
+		JOptionPane.showMessageDialog( this, "Title: " + reportData.getTitle() 
+										+ "\nType: " + reportData.getType()  
+										+ "\nData: " 
+										+ reportData.getData(), "Report",  
+										JOptionPane.INFORMATION_MESSAGE);
 	}
 
 	public void onExportPDFClick() {
+		if (reportController != null) {
+            reportController.exportReport("pdf");
+            JOptionPane.showMessageDialog(this, "Export PDF successfully!");
+        } else {
+            JOptionPane.showMessageDialog(this, "No controller attached!");
+        }
 	}
 
 	public ReportController getReportController() {
