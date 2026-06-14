5e9f57791bc14c82b7fb4c7782245e91333f28ee
24130329-huyentrang <24130329@st.hcmuaf.edu.vn>
2026-06-14 22:10:50 +0700

    Update ReportController.java

--- Soure code1/Soure code/coure_code/src/coure_code/ReportController.java
+++ Soure code1/Soure code/coure_code/src/coure_code/ReportController.java
@@ -1,18 +1,31 @@
+import java.io.File;
 import java.util.Date;
 
 public class ReportController {
 	private ReportService reportService;
+	private IReport currentReport;
 
 	public ReportController(ReportService reportService) {
 		this.reportService = reportService;
 	}
 
 	public void generateReport(String reportType, Date startDate, Date endDate) {
+		currentReport = reportService.getReport(reportType, startDate, endDate);
 	}
 
 	public void exportReport(String format) {
+		if (currentReport != null) {
+            File file = reportService.exportReport(currentReport, format);
+            System.out.println("Exported file: " + file.getAbsolutePath());
+        } else {
+            System.out.println("No report to export.");
+        }
 	}
 
+	public IReport getCurrentReport() {
+        return currentReport;
+    }
+	
 	public ReportService getReportService() {
 		return reportService;
 	}
